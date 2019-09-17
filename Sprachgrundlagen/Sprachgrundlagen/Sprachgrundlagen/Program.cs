15b188703cb07cd0cbcece98eacdaea0e95e906c
JoeVice <DanielWill@users.noreply.github.com>
2019-09-17 10:36:06 +0200

    Input/Output + wichtige Links

--- Sprachgrundlagen/Sprachgrundlagen/Sprachgrundlagen/Program.cs
+++ Sprachgrundlagen/Sprachgrundlagen/Sprachgrundlagen/Program.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Sprachgrundlagen {
     class Program {
         static void Main(string[] args) {
+
+            #region Ausgabe / Eingabe
+            // Ausgabe von Daten
+            Console.WriteLine("Hallo zusammen!");
+
+            Console.WriteLine("Bitte Name:" );
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine(Name);
+
+            Console.WriteLine("Hallo " + Name);
+            Console.WriteLine("Hallo {0}", Name);
+            Console.WriteLine($"Hallo {Name}");
+            #endregion Ausgabe / Eingabe
+
+
+            Console.WriteLine("--- ENDE PROGRAMM ---");
+            Console.ReadLine();
         }
     }
 }
