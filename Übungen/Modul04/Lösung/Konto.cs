9d9c97df83b3dc16ab4cb5d6873081dd77481f6f
JoeVice <DanielWill@users.noreply.github.com>
2019-09-18 17:03:16 +0200

    Lösungen Modul 04

---
+++ Übungen/Modul04/Lösung/Konto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontoAnwendung
+{
+    public class Konto
+    {
+        // Eigenschaften
+        public string Inhaber { get; set; }
+        public decimal Kontostand { get; private set; }
+
+        // Methoden
+        public decimal Einzahlen(decimal betrag)
+        {
+            // Logik zum Einzahlen
+            this.Kontostand += betrag;
+            return Kontostand;
+        }
+        public decimal Abheben(decimal betrag)
+        {
+            // Logik zum Abheben
+            this.Kontostand -= betrag;
+            return Kontostand;
+        }
+
+        // Konstruktor
+        public Konto(string inhaber, decimal kontostand)
+        {
+            this.Kontostand = kontostand;
+            this.Inhaber = inhaber;
+        }
+
+    }
+}
---
+++ Übungen/Modul04/Lösung/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KontoAnwendung
+{
+    class Program
+    {
+        public enum Operation { Einzahlen = 1, Abheben = 2 }
+        static void Main(string[] args)
+        {
+            Konto k1 = new Konto("Daniel Will", 10000);
+
+            Console.WriteLine("Was möchtest du machen?");
+            Console.WriteLine("1: Einzahlen / 2: Abheben");
+            int operation = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case 1:
+                    Console.WriteLine("Wieviel möchtest du einzahlen?");
+                    decimal betragE = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine($"{betragE} wurden eingezahlt");
+                    Console.WriteLine($"Neuer Kontostand beträgt: {k1.Einzahlen(betragE)}");
+                    k1.Einzahlen(betragE);
+                    break;
+                case 2:
+                    Console.WriteLine("Wieviel möchtest du abheben?");
+                    decimal betragA = decimal.Parse(Console.ReadLine());
+                    Console.WriteLine($"{betragA} wurden abgehoben");
+                    Console.WriteLine($"Neuer Kontostand beträgt: {k1.Abheben(betragA)}"); 
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("--- PROGRAMM ENDE ---");
+            Console.ReadLine();
+
+        }
+    }
+}
