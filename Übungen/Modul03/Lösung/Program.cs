fa62fb96e5f4bbced5c15dd760ceb8b5461e9e82
JoeVice <DanielWill@users.noreply.github.com>
2019-09-18 11:10:39 +0200

    Lösungen Modul03

---
+++ Übungen/Modul03/Lösung/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        public enum Rechenoperationen { Addition = 1, Subtraktion = 2, Multiplikation = 3, Division = 4}
+
+        static void Main(string[] args)
+        {
+            Console.Write("Bitte geben Sie eine Gleitkommazahl ein: ");
+            double zahl1 = double.Parse(Console.ReadLine());
+            Console.Write("Bitte geben Sie eine 2. Gleitkommazahl ein: ");
+            double zahl2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben sie die Nummer der gewünschten Operation ein: ");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraktion");
+            Console.WriteLine("3: Multiplikation");
+            Console.WriteLine("4: Division");
+
+            Rechenoperationen operation = (Rechenoperationen)int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Ergebnis: {Berechne(zahl1, zahl2, operation)}");
+            Console.ReadKey();
+        }
+
+        public static double Berechne(double zahl1, double zahl2, Rechenoperationen operation)
+        {
+            switch (operation)
+            {
+                case Rechenoperationen.Addition:
+                    return zahl1 + zahl2;
+                case Rechenoperationen.Subtraktion:
+                    return zahl1 - zahl2;
+                case Rechenoperationen.Multiplikation:
+                    return zahl1 * zahl2;
+                case Rechenoperationen.Division:
+                    if(zahl2 == 0)
+                    {
+                        Console.WriteLine("Division durch 0 ist nicht erlaubt!");
+                        return 0;
+                    }
+                    return zahl1 / zahl2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Übungen/Modul03/Lösung/Program_abstrakt.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaschenRechnerConsole {
+    class Program {
+        public enum Operation {
+            Addieren,
+            Substrahieren,
+            Multiplizieren,
+            Dividieren
+        }
+        static void Main(string[] args) {
+            Console.WriteLine("Bitte gib eine Zahl als ersten Operanden an: ");
+            double firstOperand = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bitte gib eine Zahl als zweiten Operanden an: ");
+            double secondOperand = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Welche Operation möchtest Du ausführen?");
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Substraktion");
+            Console.WriteLine("3: Multiplikation");
+            Console.WriteLine("4: Division");
+            int selectedOperation = Convert.ToInt32(Console.ReadLine());
+            switch (selectedOperation) {
+                case 1:
+                    Console.WriteLine($"Ergebnis: {Berechnen(firstOperand, secondOperand, Operation.Addieren)}"); 
+                    break;
+                case 2:
+                    Console.WriteLine($"Ergebnis: {Berechnen(firstOperand, secondOperand, Operation.Substrahieren)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"Ergebnis: {Berechnen(firstOperand, secondOperand, Operation.Multiplizieren)}");
+                    break;
+                case 4:
+                    Console.WriteLine($"Ergebnis: {Berechnen(firstOperand, secondOperand, Operation.Dividieren)}");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+        public static double Berechnen(double figOne, double figTwo, Operation operationType) {
+            switch(operationType) {
+                case Operation.Addieren:
+                    return (double)figOne + figTwo;
+                case Operation.Substrahieren:
+                    return (double)figOne - figTwo;
+                case Operation.Multiplizieren:
+                    return (double)figOne * figTwo;
+                case Operation.Dividieren:
+                    return (double)figOne / figTwo;
+                default:
+                    break;
+            }
+            return 0;
+        }
+    }
+}
