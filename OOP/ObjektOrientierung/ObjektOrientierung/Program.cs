24081473eaa7427086a31cef1716a5d3fe95b541
JoeVice <DanielWill@users.noreply.github.com>
2019-09-18 15:44:30 +0200

    OOP Beispielcode

---
+++ OOP/ObjektOrientierung/ObjektOrientierung/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektOrientierung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region werkzeug loop
+            //List<Werkzeug> werkzeugListe = new List<Werkzeug>();
+
+            //// input: anzahl Werkzeuge -> Anzahl der Objekte in Task
+            //int anzahl = 10;
+
+            //// input Werkzeug-Typ -> Bezeichnung des Objektes in Task
+
+            //for(int i = 0; i <= anzahl; i++)
+            //{
+            //    werkzeugListe.Add(new Werkzeug()
+            //    {
+            //        WerkzeugTyp = "Kreissucher"
+            //    });
+            //}
+
+            //foreach(Werkzeug werkzeug in werkzeugListe)
+            //{
+            //    if(werkzeug.Bewertung)
+            //    {
+            //        // setze grün
+            //    } else
+            //    {
+            //        // setze rot
+            //    }
+
+            #endregion werkzeug loop
+
+            Werkzeug werkzeugVariable = new Werkzeug("Kreissucher");
+
+            Werkzeug w2 = new Werkzeug("Kreissucher", 10);
+
+            Console.WriteLine("Typ von Werkzeug: " + werkzeugVariable.WerkzeugTyp);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OOP/ObjektOrientierung/ObjektOrientierung/Werkzeug.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjektOrientierung
+{
+    public class Werkzeug
+    {
+        private bool bewertung;
+
+        public bool Bewertung
+        {
+            get { return bewertung; }
+            private set { bewertung = value; }
+        }
+
+        public string WerkzeugTyp { get;  set; }
+        public int Anzahl { get; set; }
+
+        public Werkzeug(string werkzeugtyp)
+        {
+            this.bewertung = true; // Logik einbauen um Daten aus Umgebungsvariablen zu erhalten
+            this.WerkzeugTyp = werkzeugtyp;
+        }
+
+        public Werkzeug(string werkzeugtyp, int anzahl)
+        {
+            this.bewertung = true; // Logik einbauen um Daten aus Umgebungsvariablen zu erhalten
+            this.WerkzeugTyp = werkzeugtyp;
+        }
+
+    }
+}
